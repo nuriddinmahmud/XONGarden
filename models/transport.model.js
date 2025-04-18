42314b49bcb870a3763d2fa4032a3d0418dc59b7
Nuriddin Mahmud <154328556+nuriddinmahmud@users.noreply.github.com>
2025-04-19 00:37:44 +0500

    created sequelize

---
+++ models/transport.model.js
@@ -0,0 +1,27 @@
+import { DataTypes } from 'sequelize';
+import sequelize from '../config/database.js'; 
+
+const Transport = sequelize.define('Transport', {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  date: {
+    type: DataTypes.DATE,
+    allowNull: false,
+  },
+  transportType: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  comment: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+}, {
+  tableName: 'transports',
+  timestamps: false,
+});
+
+export default Transport;
